ffa9a509fe83d6a8e91303b765cb8ce650859453
ah100101 <ah100101@gmail.com>
2019-01-26 18:05:03 -0600

    ga updates and cleanup

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,9 +1,18 @@
+const gaId = process.env.GA_CODE || 'UA-ID'
+
 module.exports = {
   title: 'BlockScoped',
   description: 'Random coding questions by topic and difficulty',
   head: [
     ['link', { rel: 'icon', href:'/images/logo.png' }],
-    ['link', { rel: 'manifest', href:'/manifest.json' }]
+    ['link', { rel: 'manifest', href:'/manifest.json' }],
+    ['script', { src: `https://www.googletagmanager.com/gtag/js?id=${gaId}` }],
+    ['script', {}, `
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+      gtag('config', '${gaId}', { 'anonymize_ip': true });
+    `]
   ],
   serviceWorker: true,
   themeConfig: {
@@ -21,14 +30,19 @@ module.exports = {
         ]
       },
       {
-        text: 'About',
+        text: 'Contributing',
         items: [
-          { text: 'Contributing', link: '/about/contributing/' },
-          { text: 'Privacy Policy', link: '/about/privacy/' },
-          { text: 'Contact', link: '/about/contact/' },
+          { text: 'Overview', link: '/contributing/' },
           { text: 'Request New Question Topic', link: 'https://github.com/ah100101/BlockScoped/issues/new?title=New+Topic+Request:[topic%20name]&labels=enhancement&assignee=ah100101' },
           { text: 'Submit Issue', link: 'https://github.com/ah100101/BlockScoped/issues/new'},
           { text: 'View Current Issues', link: 'https://github.com/ah100101/BlockScoped/issues' }
+        ]
+      },
+      {
+        text: 'About',
+        items: [
+          { text: 'Privacy Policy', link: '/about/privacy/' },
+          { text: 'Contact', link: '/about/contact/' }
         ]
       }
     ],
